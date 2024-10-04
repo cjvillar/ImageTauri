9ab839bf450335351e6e5a891211cd0017dd08a9
Chris Villarreal <cjvillar@users.noreply.github.com>
2024-10-04 14:29:20 -0700

    update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,19 +4,11 @@ mod commands;
 //imports
 use commands::file_picker::pick_file; 
 
-#[tauri::command]
-fn my_custom_command(invoke_message: String) -> String {
-    format!(
-        "I was invoked from JS, with this message: {}",
-        invoke_message
-    )
-}
-
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(tauri::generate_handler![my_custom_command, pick_file])
+        .invoke_handler(tauri::generate_handler![pick_file])
         .run(tauri::generate_context!())
         .expect("failed to run app");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,8 @@ import { convertFileSrc } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [message, setName] = useState("");
-  const [setImagePath] = useState("");
+  const [imgPath, setImagePath] = useState("");
   const [imgUrl, setImgUrl] = useState(""); // State to store converted image URL
-
-  async function greet() {
-    setGreetMsg(await invoke("my_custom_command", { invokeMessage: message }));
-  }
 
   async function selectImage() {
     const result = await invoke("pick_file");
@@ -27,40 +21,20 @@ function App() {
 
   return (
     <>
-      <div className="container">
-        <h1>ImageTauri</h1>
-        <form
-          className="row"
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
+      <button className="button" onClick={selectImage}>
+        Select JPEG Image From Desktop
+      </button>
+
+      {imgUrl && ( // use imgUrl instead of imagePath
+        <div className="selected-image-container">
+          <h2>Selected Image:</h2>
+          <img
+            src={imgUrl}
+            alt="Selected"
+            // style={{ maxWidth: "100%", height: "auto" }}
           />
-          <button type="submit">Greet</button>
-        </form>
-
-        <button className="button" onClick={selectImage}>
-          Select JPEG Image
-        </button>
-
-        {imgUrl && ( // use imgUrl instead of imagePath
-          <div className="selected-image-container">
-            <h2>Selected Image:</h2>
-            <img
-              src={imgUrl} // corrected string interpolation
-              alt="Selected"
-              style={{ maxWidth: "100%", height: "auto" }}
-            />
-          </div>
-        )}
-
-        <p>{greetMsg}</p>
-      </div>
+        </div>
+      )}
     </>
   );
 }
