148a21a9f956c9eec79ab4bc75d21ac5f9cdf330
Chris Villarreal <cjvillar@users.noreply.github.com>
2024-10-06 11:26:04 -0700

    update ui

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import SideBar from "./sideBar";
+import "../App.css";
+
+function App() {
+  const [imgPath, setImagePath] = useState(""); // state to store image path
+  const [imgUrl, setImgUrl] = useState(""); // state to store image URL
+
+  return (
+    <div className="app-container">
+      {/* Sidebar */}
+      <SideBar
+        imgPath={imgPath}
+        imgUrl={imgUrl}
+        setImagePath={setImagePath}
+        setImgUrl={setImgUrl}
+      />
+
+      {/* image display */}
+      <div className="main-content">
+        {imgUrl ? (
+          <div className="selected-image-container">
+            <img src={imgUrl} alt="Selected" />
+            <p style={{ fontSize: "12px", color: "grey" }}>{imgPath}</p>
+          </div>
+        ) : (
+          <p>Select an image from the sidebar to display it here.</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
